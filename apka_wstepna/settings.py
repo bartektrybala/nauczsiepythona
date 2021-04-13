1be6fdf8a82f6ccfdc5a54f90dd547e22e349d5e
BartekApka <bartektrybalaa@gmail.com>
2021-04-13 15:01:33 +0000

    pythonanywhere files

--- apka_wstepna/settings.py
+++ apka_wstepna/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'x+y9)5qt_0zvnrcj(60cp3(bgpihj(=2vdj)%fe&i73i=@s7i7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
